752d4de058679c1ea5b836feaaca37c23e6b2c14
Soheib Biga <soheib.biga@outlook.fr>
2016-03-24 15:52:57 +0100

    Jring : First implementation

---
+++ java/src/Jring.java
@@ -0,0 +1,104 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Jring{
+    static entity ent;
+    
+    public static void main(String[] args){
+        InetAddress a =host_ip();
+        System.out.println(a.toString());
+        System.out.println("Give your id");
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine();
+        while(id.length()>8){
+            System.out.println("The maximum length of your id is 8 characters");
+            id = sc.nextLine();
+        }
+        ent = new entity(id);
+        if(args.length==0){
+            ent.mdiff_ip="255.255.255.255";
+            ent.mdiff_port=4444;
+        }
+        else{            
+            insert(args[0],Integer.parseInt(args[1]));
+        }
+        try{
+            ServerSocket server = new ServerSocket(ent.tcp);
+            while(true){
+                Socket socket = server.accept();
+                Service_tcp serv_tcp = new Service_tcp(socket,ent);
+                Thread t = new Thread(serv_tcp);
+                t.start();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+    
+
+    public static void insert(String adress,int tcp){
+        try{
+            Socket socket = new Socket(adress,tcp);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            String mess_recv=br.readLine();
+            welc_mess data_enter = parse_welc(mess_recv);
+            ent.ip_next=data_enter.ip_next;
+            ent.port_next=data_enter.port_next;
+            ent.mdiff_ip=data_enter.mdiff_ip;
+            ent.mdiff_port=data_enter.mdiff_port;
+            String mess_send = "NEWC "+host_ip().toString()+" "+ent.udp;
+            pw.println(mess_send);
+            mess_recv=br.readLine();
+            if(mess_recv.equals("ACKC")){
+                System.out.println("Connexion Succeed !");
+            }
+            br.close();
+            pw.close();
+            socket.close();
+        }catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+
+    public static InetAddress host_ip(){
+        try{
+            Enumeration<NetworkInterface> listNi=NetworkInterface.getNetworkInterfaces();
+            boolean t=false;
+            InetAddress iac=null;
+            while(listNi.hasMoreElements()){
+                NetworkInterface nic=listNi.nextElement();
+                Enumeration<InetAddress> listIa=nic.getInetAddresses();
+                while(listIa.hasMoreElements()){
+                    iac=listIa.nextElement();
+                    if(iac instanceof Inet4Address && !iac.isLoopbackAddress()){
+                        return iac;
+                    }
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static welc_mess parse_welc(String mess){
+        mess=mess.substring(0,mess.length()-1);
+        String []tab = mess.split(" ");
+        if(tab.length!=5 || !tab[0].equals("WELC")){
+            System.out.println("The message doesn't have the right structure (1)");
+            return null;
+        }
+        try{
+            return new welc_mess(tab[1],Integer.parseInt(tab[2]),tab[3],Integer.parseInt(tab[4]));
+        }
+        catch(Exception e){
+            System.out.println("The message doesn't have the right structure (2)");
+            return null;
+        }
+    }
+}
---
+++ java/src/welc_mess.java
@@ -0,0 +1,13 @@
+public class welc_mess{
+    String ip_next;
+    int port_next;
+    String mdiff_ip;
+    int mdiff_port;
+
+    public welc_mess(String ip_n,int port_n,String md_ip,int md_port){
+        ip_next=ip_n;
+        port_next=port_n;
+        mdiff_ip=md_ip;
+        mdiff_port=md_port;
+    }
+}
