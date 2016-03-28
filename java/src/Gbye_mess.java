4939280f6382c90a27add32729e9401d2e871ed2
Soheib Biga <soheib.biga@outlook.fr>
2016-03-28 18:55:59 +0200

    Loop of a message

---
+++ java/src/Gbye_mess.java
@@ -0,0 +1,15 @@
+public class Gbye_mess{
+    String idm;
+    String ip;
+    int port;
+    String ip_next;
+    int port_next;
+
+    public Gbye_mess(String _idm,String ip_s,int port_s,String ip_n,int port_n){
+        idm=_idm;
+        ip=ip_s;
+        port=port_s;
+        ip_next=ip_n;
+        port_next=port_n;
+    }
+}
--- java/src/Jring.java
+++ java/src/Jring.java
@@ -4,21 +4,21 @@ import java.io.*;
 
 public class Jring{
     static Entity ent;
-    // static ArrayList<String> mess_list;
+    static ArrayList<String> mess_list;
     
     public static void main(String[] args){
-        //mess_list = new
+        mess_list = new ArrayList<String>();
         InetAddress a =host_ip();
         System.out.println(a.toString());
         System.out.println("Give your id");
         Scanner sc = new Scanner(System.in);
         String id = sc.nextLine();
-        System.out.println("Give your tcp port");
-        int k = sc.nextInt();
         while(id.length()>8){
-            System.out.println("The maximum length of your id is 8 characters");
+            System.out.println("The maximum length of your id is 8 characters \n Give a new id");
             id = sc.nextLine();
         }
+        System.out.println("Give your tcp port");
+        int k = sc.nextInt();
         ent = new Entity(id,k);
         if(args.length==0){
             ent.mdiff_ip="255.255.255.255";
@@ -33,7 +33,7 @@ public class Jring{
             Thread tcp_t = new Thread(tcp_mode);
             tcp_t.start();
             //Udp
-            Udp_thread udp_mode = new Udp_thread(ent,a);
+            Udp_thread udp_mode = new Udp_thread(ent,a,mess_list);
             Thread udp_t = new Thread(udp_mode);
             udp_t.start();
             //Send a new message
@@ -48,9 +48,10 @@ public class Jring{
                 packet_send = new DatagramPacket(data,data.length,next_ia);
                 dso.send(packet_send);
                 String []tab = mess_send.split(" ");
-                if(tab.length==6 && tab[0].equals("GBYE")){
-                    
-                }
+                int type=type_mess(tab[0]);
+                if(type==2){
+                    if(parse_gbye(tab,true)!=null) mess_list.add(tab[0]);
+                }              
             }
         }catch(Exception e){
             System.out.println(e);
@@ -76,7 +77,7 @@ public class Jring{
                 pw.flush();
                 mess_recv=br.readLine();
                 if(mess_recv.equals("ACKC")){
-                    System.out.println("Connexion Succeed !");
+                    System.out.println("insert : Connexion Succeed !");
                 }
             }
             br.close();
@@ -116,15 +117,44 @@ public class Jring{
         //System.out.println("apres sub "+mess);
         String []tab = mess.split(" ");
         if(tab.length!=5 || !tab[0].equals("WELC")){
-            System.out.println("The message doesn't have the right structure (1)");
+            System.out.println("parse_welc : The message doesn't have the right structure (1)");
             return null;
         }
         try{
             return new Welc_mess(tab[1],Integer.parseInt(tab[2]),tab[3],Integer.parseInt(tab[4]));
         }
         catch(Exception e){
-            System.out.println("The message doesn't have the right structure (2)");
+            System.out.println("parse_welc : The message doesn't have the right structure (2)");
             return null;
         }
+    }
+
+    public static Gbye_mess parse_gbye(String[] tab,boolean need_checking){
+        if(!need_checking) return new Gbye_mess(tab[1],tab[2],Integer.parseInt(tab[3]),tab[4],Integer.parseInt(tab[5]));
+        int port,port_next;
+        if(tab.length!=6 || !tab[0].equals("GBYE")){
+            System.out.println("parse_gbye : The message doesn't have the right structure");
+            return null;
+        }
+        if(tab[1].length()>8){
+            System.out.println("parse_gbye : The identify of a message have 8 characters maximum");
+            return null;
+        }
+        try{
+            port=Integer.parseInt(tab[3]);
+            port_next=Integer.parseInt(tab[5]);
+        }
+        catch(Exception e){
+            System.out.println("parse_gbye : The message doesn't have the right structure, you don't give number for port and/or port_next'");
+            return null;
+        }
+        return new Gbye_mess(tab[1],tab[2],port,tab[4],port_next);
+    }
+
+    public static int type_mess(String type){
+        if(type.equals("WHOS")) return 1;
+        if(type.equals("GBYE")) return 2;
+        if(type.equals("Test")) return 3;
+        return 0;
     }
 }
