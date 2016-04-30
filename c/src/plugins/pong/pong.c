7db36c91f1c79c1c7d93e8edbb3b92478be4d56a
Younes Megrini <rasetoi@yahoo.fr>
2016-04-30 15:02:27 +0200

    pong.c: plugin basic setup source file

--- c/src/plugins/pong/pong.c
+++ c/src/plugins/pong/pong.c
@@ -2,4 +2,49 @@
 #include "../../plugin_system/protocol_interface.h"
 
 static int cmd_pong(int argc, char **argv);
-static int action_pong(const char *message, const chat* content, int lookup_flag);
+static int action_pong(const char *message, const char* content, int lookup_flag);
+static void close_pong();
+
+PluginCommand_t pcmd_pong = {
+    "pong",
+    "Classic multiplayer tennis game running over the network.",
+    cmd_pong
+};
+
+PluginAction_t paction_pong = {
+    "PONG####",
+    "Network packet for 'pong' application.",
+    action_pong
+};
+
+Plugin plugin_pong = {
+    1,
+    &pcmd_pong,
+    1,
+    &paction_pong,
+    close_pong
+};
+
+
+int init_pong(PluginManager *p)
+{
+    return plugin_register(p, "pong", &plugin_pong);
+}
+
+
+
+
+static int cmd_pong(int argc, char **argv) 
+{
+    return 0;
+}
+
+static int action_pong(const char *message, const char *content, int lookup_flag)
+{
+    return 0;
+}
+
+static void close_pong(){
+
+    return;
+}
