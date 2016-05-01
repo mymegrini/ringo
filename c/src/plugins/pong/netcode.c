86833588fbda3283ad9fe3f7c6ea5688038f9c83
Younes Megrini <rasetoi@yahoo.fr>
2016-05-01 13:54:21 +0200

    netcode: library for handling network communication

---
+++ c/src/plugins/pong/netcode.c
@@ -0,0 +1,18 @@
+
+/**
+ * This function parses pong application messages
+ */
+int parsePong(const char* message, const char* content, int lookup_flag){
+
+    printf("%d\n", content-message);
+    return 0;
+}
+
+/**
+ * This function tries to establish a connection to another pong application
+ */
+int
+connect(){
+
+    return 0;
+}
---
+++ c/src/plugins/pong/netcode.h
@@ -0,0 +1,7 @@
+#ifndef NETCODE_H
+#define NETCODE_H
+
+int parsePong(const char *message, const char *content, int lookup_flag);
+int connect();
+
+#endif
